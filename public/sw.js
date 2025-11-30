16f449508ec51f6705b0ade0fb460e16d8c4a4ba
pgatour29-pro <pgatour29@gmail.com>
2025-12-01 00:11:53 +0700

    Deploy simple eq query with debug logging

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // SERVICE WORKER - UNREGISTRATION VERSION
 // This version clears all caches and unregisters itself
 
-const SW_VERSION = 'default-pin-6789-v8-query-both-fields';
+const SW_VERSION = 'default-pin-6789-v9-simple-eq-query';
 
 self.addEventListener('install', event => {
     console.log('[ServiceWorker] Installing unregistration version');

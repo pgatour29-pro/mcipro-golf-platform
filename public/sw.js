80a430289b19c183217e132cb7a0f24f453e877f
pgatour29-pro <pgatour29@gmail.com>
2025-12-23 12:46:50 +0700

    debug: Add logging to registration real-time subscription

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // SERVICE WORKER - UNREGISTRATION VERSION
 // This version clears all caches and unregisters itself
 
-const SW_VERSION = 'new-registration-badge-v1';
+const SW_VERSION = 'new-registration-badge-v2';
 
 self.addEventListener('install', event => {
     console.log('[ServiceWorker] Installing unregistration version');

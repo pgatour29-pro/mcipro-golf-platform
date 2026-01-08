10f10ad49af001112fe6aa54597391cb18b8285f
pgatour29-pro <pgatour29@gmail.com>
2026-01-09 06:08:19 +0700

    Add visible debug alert for state mismatch

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // SERVICE WORKER - Performance Caching Version
 // Caches static assets for dramatically faster repeat visits
 
-const SW_VERSION = 'mcipro-cache-v52';
+const SW_VERSION = 'mcipro-cache-v53';
 const CACHE_NAME = `mcipro-static-${SW_VERSION}`;
 const RUNTIME_CACHE = `mcipro-runtime-${SW_VERSION}`;
 
